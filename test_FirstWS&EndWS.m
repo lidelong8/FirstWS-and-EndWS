1403b74ee66795a104cc37ffcbf1d7002d65de58
lidelong8 <133221050+lidelong8@users.noreply.github.com>
2023-05-11 20:16:29 +0800

    Update test_FirstWS&EndWS.m

--- test_FirstWS&EndWS.m
+++ test_FirstWS&EndWS.m
@@ -1,32 +1,14 @@
-%%%%%%%%%%%%%%
-x=1900:2099;
-rand('seed',10);
-y1=rand(200,1);
-y1=y1*100;
-x0=linspace(1,10,200)';
-y2=-(x0-3).*(x0-5.5).*(x0-9);
-y3=934;
-y=y1+y2+y3+2;
-y(182:end)=y(182:end)+60;
-y(1:150)=y(1:150)+30;
-a=y(1990-1900:2050-1900);
-b=1000+rand(size(a))*10;
-y(1990-1900:2050-1900)=b;
-data=y;
-save('D:\ToE\output_data\test_pixel_data.mat','data');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-cd D:\ToE\figures
+cd D:\ToE\figures %Change the workong space
 load('test_pixel_data.mat','data');
 %%smooth  running mean
-width=11; %the moving-window years
-data=nanfastsmooth(data,width,1);
-%data=smooth(data,width);
+width=11; %the moving-window years, you can change it!
+data=smooth(data,width);
 %%smooth
 
 %%cal FirstWS and EndWS
 tic
-[ToE,ToD]=Cal_WS(data,1000)
+[ToE,ToD]=Cal_WS(data,1000)  %the threshold of water scarcity, you can change it!
 toc %the executing time for only one pixel or a watershed
 %%cal FirstWS and EndWS
 
@@ -38,8 +20,8 @@ figure('unit','centimeters','Position',[0 0 55 30]);
 h1=plot(x(1:end-10),y(1:end-10),'c','Linewidth',1);
 hold on
 line([1900 2090],[1000 1000])
-set(gca,'XMinorTick','on')%最小刻度
-set(gca,'YMinorTick','on')%最小刻度
+set(gca,'XMinorTick','on')
+set(gca,'YMinorTick','on')
 %%%%%%%%%%%%%%
 xi = 1900:0.1:2089;
 yi = interp1(x(1:end-10),data(1:end-10),xi,'linear'); %only show 19000-2090
@@ -53,7 +35,6 @@ ylim = get(gca,'YLim');
 x1=0+(ToE-xlim(1))/diff(xlim);
 x2=0+(ToD-xlim(1))/diff(xlim);
 % Add labels
-%
 line([ToE ToE],[1000 1.01*mean(ylim)],'linestyle','-','color','r','Linewidth',2.0)
 text(ToE,1.01*mean(ylim),num2str(ToE),'rotation',90,'Color','r','FontName','Arial','FontSize',14,'fontweight','bold');
 %
@@ -82,4 +63,5 @@ lgd.Box='off';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %save pic
 exportgraphics(gcf,'test_FirstWS&EndWS.jpg','Resolution',350);% no white 
+disp('finished!')
 %%%%%%%%%%%%%%%%%%%%%
